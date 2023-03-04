85042b85d38cfe0ce67357f7288c41820bf27709
Kimbumsoo99 <show7441@naver.com>
2023-03-05 03:15:32 +0900

    연습

--- modern_JS/11/es2015.js
+++ modern_JS/11/es2015.js
@@ -19,3 +19,25 @@ obj1["this blank"] = "첨 봄";
 console.log(obj1);
 
 // Symbol() 을 사용한 키는 forEach 같은것으로 접근이 안됨 (외부 공개 X)
+// .map() 을 통해 배열 요소에 함수 적용 후 배열 반환 가능
+const numbers1 = [1, 2, 3, 4, 5];
+const newNumbers1 = numbers1.map((num, i) => i + num * 3);
+console.log(newNumbers1);
+
+//filter() 사용 시 배열 함수를 받아서 특정 조건 고를 수 있음
+const scores = [90, 35, 64, 88, 43, 52];
+const highScores = scores.filter((score, index) => {
+  if (score >= 85) {
+    console.log(`index : ${index}, score : ${score}`);
+    return score;
+  }
+});
+console.log(highScores);
+//값 하나로 누적하는 .reduce() 메서드
+
+const numbers2 = [1, 2, 3, 4, 5];
+const result2 = numbers2.reduce((total, current) => total + current, 8); //8은 초기값
+const result1 = numbers2.reduce((total, current) => total + current, 0);
+
+console.log(result2);
+console.log(result1);
