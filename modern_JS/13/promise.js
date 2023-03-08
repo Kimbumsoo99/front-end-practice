cb9e02eac36379166473d17a1d399479738b3525
Kimbumsoo99 <show7441@naver.com>
2023-03-08 16:39:29 +0900

    연습

--- modern_JS/13/promise.js
+++ modern_JS/13/promise.js
@@ -6,11 +6,53 @@ const pizza = new Promise((resolve, reject) => {
   } else reject("피자를 주문하지 않습니다.");
 });
 
-pizza
-  .then((result) => console.log(result))
-  .catch((err) => console.error(err))
-  .finally(() => console.log("완료"));
+const promiseChain = () => console.log("피자를 만들고 있습니다.");
 
 // promise 대기 상태 pending
 // 처리 성공 상태 fulfilled
 // 처리 실패 상태 rejected
+
+const step1 = (msg) => {
+  console.log(msg);
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve("피자 도우 준비");
+    }, 3000);
+  });
+};
+const step2 = (msg) => {
+  console.log(msg);
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve("토핑 완료");
+    }, 1000);
+  });
+};
+const step3 = (msg) => {
+  console.log(msg);
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve("굽기 완료");
+    }, 2000);
+  });
+};
+
+const pizza2 = () => {
+  return new Promise((resolve, reject) => {
+    resolve("피자2를 주문합니다.");
+  });
+};
+
+pizza
+  .then((result) => console.log(result))
+  .then(promiseChain)
+  .catch((err) => console.error(err))
+  .finally(() => console.log("완료"));
+pizza2()
+  .then((result) => step1(result))
+  .then((result) => step2(result))
+  .then((result) => step3(result))
+  .then((result) => console.log(result))
+  .then(() => {
+    console.log("피자가 준비되었습니다.");
+  });
